601ff681bc17b8403b13cff845c44191a5eaaaae
1013180 <hiroki.kojima0417@gmail.com>
2021-02-03 10:04:18 +0000

    [Update:KADAI3]

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,8 +3,11 @@ class PostCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = @book.id
-    comment.save
-    redirect_to request.referer
+    if comment.save
+        redirect_to request.referer
+    else
+        redirect_to request.referer
+    end
   end
 
 
