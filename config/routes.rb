98c43fbebc93b0d273bf2661ed0a0cf38c67b8d6
1013180 <hiroki.kojima0417@gmail.com>
2021-02-06 09:29:56 +0000

    [Finish:KADAI5]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+#   get 'seraches/search'
    root 'homes#top'
    devise_for :users
    get 'home/about' => 'homes#about'
+
    get 'users/:id/show_follow' => 'users#show_follow', as: "users_show_follow"
    get 'users/:id/show_follower' => 'users#show_follower', as: "users_show_follower"
+
+   get 'users/search' => 'searches#search'
    resources :users,only: [:show,:index,:edit,:update]
 
    resources :relationships, only: [:create, :destroy]
